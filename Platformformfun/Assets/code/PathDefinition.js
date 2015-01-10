5ccf9d19bb034fdd29aabf09153ed57796e362db
Ívan Bjarni Jónsson <ibj9@hi.is>
2015-01-10 13:30:42 +0000

    Playground

---
+++ Platformformfun/Assets/code/PathDefinition.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var points : Transform[] = null;
+
+function OnDrawGizmos() 
+{			
+	if(!points || points.length<2)
+		return;
+	
+	for(var i=1; i<points.length;i++)
+	{
+		Gizmos.DrawLine(points[i-1].position, points[i].position);
+	}
+}
